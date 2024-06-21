124140a2737994789f36bcf6d5133405dd5c4ff5
PehJiaXuan <133181953+PehJiaXuan@users.noreply.github.com>
2024-06-22 00:36:20 +0800

    Delete unused file (server/radio/index.js)

--- MeTime/server/radio/index.js
+++
@@ -1,8 +0,0 @@
-// radio
-const axios = require('axios');
-
-const querystr = `https://de1.api.radio-browser.info/json/stations/bycountry/Malaysia`;
-const app = require('express')
-app.use(express.json())
-
-// app.get('/myapi'
